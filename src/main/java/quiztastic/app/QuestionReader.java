0ae5b412c9c8327d62389b7f281520c91ed91124
dyrhoi <realarcoz@gmail.com>
2020-08-28 11:15:14 +0200

    Exception handlers.

--- src/main/java/quiztastic/app/QuestionReader.java
+++ src/main/java/quiztastic/app/QuestionReader.java
@@ -5,12 +5,14 @@ import quiztastic.core.Question;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.text.ParseException;
 
 /**
  * The Question Reader should read the questions from a file.
  */
 public class QuestionReader {
     private final BufferedReader reader;
+    private int lineCounter;
 
     public QuestionReader(BufferedReader reader) {
         this.reader = reader;
@@ -20,20 +22,29 @@ public class QuestionReader {
         this(new BufferedReader(reader));
     }
 
-    public Question readQuestion() throws IOException, NumberFormatException {
+    public Question readQuestion() throws IOException, ParseException, NumberFormatException {
         //throw new UnsupportedOperationException("Not yet implemented");
+        lineCounter++;
+
         String line = reader.readLine();
         if(line == null)
             return null;
 
-        String[] questionData = line.split("\t");
-        if(questionData.length != 4)
-            return null;
+        String[] questionFields = line.split("\t");
+        if(questionFields.length != 4)
+            throw new ParseException("Expected 4 fields, received: " + questionFields.length, lineCounter);
 
-        int score = Integer.parseInt(questionData[0]);
-        String category = questionData[1];
-        String question = questionData[2];
-        String answer = questionData[3];
+        int score;
+        try {
+            score = Integer.parseInt(questionFields[0]);
+        }
+        catch (NumberFormatException e) {
+            throw new ParseException("Expected int in field 1, but received \"" + questionFields[0] + "\"",
+                    lineCounter);
+        }
+        String category = questionFields[1];
+        String question = questionFields[2];
+        String answer = questionFields[3];
 
         return new Question(score, category, question, answer);
     }
--- src/test/java/quiztastic/app/QuestionReaderTest.java
+++ src/test/java/quiztastic/app/QuestionReaderTest.java
@@ -7,6 +7,7 @@ import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,30 +35,49 @@ class QuestionReaderTest {
     }
 
     @Test
-    void shouldReadSingleQuestion() throws IOException {
+    void shouldReadSingleQuestion() throws IOException, ParseException {
         String questionText = "100\tLAKES & RIVERS\tRiver mentioned most often in the Bible\tthe Jordan\n";
         QuestionReader reader = new QuestionReader(new StringReader(questionText));
         Question q = reader.readQuestion();
         assertNotNull(q);
         // Insert more tests
-        assertEquals(q.getScore(), 100);
-        assertEquals(q.getCategory(), "LAKES & RIVERS");
-        assertEquals(q.getQuestion(), "River mentioned most often in the Bible");
-        assertEquals(q.getAnswer(), "the Jordan");
+        assertEquals(100, q.getScore());
+        assertEquals("LAKES & RIVERS", q.getCategory());
+        assertEquals("River mentioned most often in the Bible", q.getQuestion());
+        assertEquals("the Jordan", q.getAnswer());
 
         Question end = reader.readQuestion();
         assertNull(end);
     }
 
     @Test
-    void shouldReadManyQuestions() throws IOException {
+    void shouldThrowParseExceptionTooFewFields() throws IOException {
+        String questionText = "100\tLAKES & RIVERS\tthe Jordan\n";
+        QuestionReader qr = new QuestionReader(new StringReader(questionText));
+        ParseException e = assertThrows(ParseException.class, () -> {
+            qr.readQuestion();
+        });
+        assertEquals("Expected 4 fields, received: 3", e.getMessage());
+    }
+
+    @Test
+    void shouldThrowParseExceptionBadInteger() throws IOException {
+        String questionText = "xxx\tLAKES & RIVERS\tQuestion\tthe Jordan\n";
+        QuestionReader qr = new QuestionReader(new StringReader(questionText));
+        ParseException e = assertThrows(ParseException.class, () -> {
+            qr.readQuestion();
+        });
+        assertEquals("Expected int in field 1, but received \"xxx\"", e.getMessage());
+    }
+
+    @Test
+    void shouldReadManyQuestions() throws IOException, ParseException {
         InputStream s = this.getClass()
                 .getClassLoader()
                 .getResourceAsStream("questions-small.tsv");
         if (s == null) fail();
 
-        QuestionReader reader = new QuestionReader(
-                new InputStreamReader(s));
+        QuestionReader reader = new QuestionReader(new InputStreamReader(s));
         int count = 0;
         while (reader.readQuestion() != null) {
             count++;
