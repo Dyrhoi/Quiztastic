60bcbe98ee2e15e6daa6d9e4d8bb26a582ce080a
Dyrhoi <realarcoz@gmail.com>
2020-08-31 16:07:50 +0200

    Initial board creation functionality.

--- src/main/java/quiztastic/core/Board.java
+++ src/main/java/quiztastic/core/Board.java
@@ -1,7 +1,39 @@
 package quiztastic.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** A Jeopardy Board
  *
  */
 public class Board {
+    private final List<Group> groups;
+
+    public Board(List<Group> groups) {
+        this.groups = new ArrayList<>(groups);
+        if(groups.size() != 6)
+            throw new IllegalArgumentException("Board should contain 6 question groups, received  " + groups.size());
+    }
+
+    public static class Group {
+        private final Category category;
+        private final List<Question> questions;
+
+        public Group(Category category, List<Question> questions) {
+            this.category = category;
+            this.questions = new ArrayList<>(questions);
+            validate();
+
+        }
+        private void validate() {
+            if(questions.size() != 5)
+                throw new IllegalArgumentException("Board group should contain 5 questions, received  " + questions.size());
+
+            for(Question q : questions) {
+                if(q.getCategory() != this.category)
+                    throw new IllegalArgumentException("Expected all question categories to be "
+                            + this.category + " received" + q.getCategory());
+            }
+        }
+    }
 }
---
+++ src/main/java/quiztastic/domain/BoardController.java
@@ -0,0 +1,14 @@
+package quiztastic.domain;
+
+import quiztastic.core.Board;
+
+public class BoardController {
+    public Board.Group makeGroup() {
+        return null;
+    }
+
+    public Board makeBoard() {
+
+        return null;
+    }
+}
